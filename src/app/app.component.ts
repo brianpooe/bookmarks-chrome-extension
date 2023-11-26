603360057980fb113340f249b37ff7731381acbe
Brian Pooe <bria.method@gmail.com>
2023-11-26 12:56:08 +0200

    feat: update bookmark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import BookmarkTreeNode = chrome.bookmarks.BookmarkTreeNode;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
   template: `
       <div class="container">
           <h1>{{ title }}</h1>
           <div class="bookmark">
               @for (item of bookmarks;track item.id) {
                   <div class="bookmark__item">
-                      <h3><span class="unique-identifier">{{ item.id }}.</span> <a [href]="item.url"
-                                                                                   target="_blank">{{ item.title }}</a>
-                      </h3>
+                      <span class="unique-identifier">{{ item.id }}.</span>
+                      @if (!item.isEditable) {
+                          <p>
+                              <a [href]="item.url" target="_blank">{{ item.title }}</a>
+                          </p>
+                      } @else {
+                          <p class="editor">
+                              <input [(ngModel)]="editedItemTitle"
+                                     [value]="item.title"
+                                     type="text"
+                                     name="edit" id="edit">
+                              <button type="button" (click)="update(item)">update</button>
+                          </p>
+                      }
+                      <svg
+                              (click)="toggleEditor(item)"
+                              width="24"
+                              height="24"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                      >
+                          <path
+                                  fill-rule="evenodd"
+                                  clip-rule="evenodd"
+                                  d="M21.2635 2.29289C20.873 1.90237 20.2398 1.90237 19.8493 2.29289L18.9769 3.16525C17.8618 2.63254 16.4857 2.82801 15.5621 3.75165L4.95549 14.3582L10.6123 20.0151L21.2189 9.4085C22.1426 8.48486 22.338 7.1088 21.8053 5.99367L22.6777 5.12132C23.0682 4.7308 23.0682 4.09763 22.6777 3.70711L21.2635 2.29289ZM16.9955 10.8035L10.6123 17.1867L7.78392 14.3582L14.1671 7.9751L16.9955 10.8035ZM18.8138 8.98525L19.8047 7.99429C20.1953 7.60376 20.1953 6.9706 19.8047 6.58007L18.3905 5.16586C18 4.77534 17.3668 4.77534 16.9763 5.16586L15.9853 6.15683L18.8138 8.98525Z"
+                                  fill="currentColor"
+                          />
+                          <path
+                                  d="M2 22.9502L4.12171 15.1717L9.77817 20.8289L2 22.9502Z"
+                                  fill="currentColor"
+                          />
+                      </svg>
                       <svg
                               (click)="delete(item)"
                               width="24"
@@ -38,6 +69,7 @@ import BookmarkTreeNode = chrome.bookmarks.BookmarkTreeNode;
                           />
                       </svg>
                   </div>
+
               } @empty {
                   <p class="bookmark__item">There are no bookmarks.</p>
               }
@@ -60,7 +92,7 @@ import BookmarkTreeNode = chrome.bookmarks.BookmarkTreeNode;
           &__item {
             width: 100%;
             display: grid;
-            grid-template-columns: 1fr 50px;
+            grid-template-columns: 50px 1fr 50px 50px;
             align-items: center;
 
             svg {
@@ -74,6 +106,10 @@ import BookmarkTreeNode = chrome.bookmarks.BookmarkTreeNode;
             .unique-identifier {
               padding: 0 1.5em;
             }
+
+            .editor {
+              padding: 0 1.5em;
+            }
           }
         }
       }
@@ -83,6 +119,7 @@ import BookmarkTreeNode = chrome.bookmarks.BookmarkTreeNode;
 export class AppComponent implements OnInit {
   title = 'Chrome bookmarks extension';
   bookmarks: BookmarkNode[] = [];
+  editedItemTitle: string;
 
   ngOnInit(): void {
     (async () => {
@@ -93,11 +130,17 @@ export class AppComponent implements OnInit {
 
   extractAllChildren(data: BookmarkTreeNode[]): BookmarkNode[] {
     let extractedData: BookmarkNode[] = [];
-    let stack: BookmarkTreeNode[][] = [data]; // Initialize the stack with the top-level nodes
+    // Create a stack to keep track of nodes to be processed
+    let stack: BookmarkTreeNode[][] = [data];
 
+    // Process nodes until the stack is empty
     while (stack.length > 0) {
-      const currentChildren: BookmarkTreeNode[] = stack.pop(); // Pop the next level of nodes from the stack
+      // Get the next level of nodes from the stack
+      const currentChildren: BookmarkTreeNode[] = stack.pop();
+
+      // Process each child node
       for (const child of currentChildren) {
+        // If the child node has a URL, add child to the extracted data
         if (child.url) {
           extractedData = [
             ...extractedData,
@@ -108,8 +151,11 @@ export class AppComponent implements OnInit {
               parentId: child.parentId,
               title: child.title,
               url: child.url,
+              isEditable: false,
             },
           ];
+
+          // If the child node has children, add them to the stack for processing
         } else if (child.children) {
           stack = [...stack, child.children];
         }
@@ -123,7 +169,25 @@ export class AppComponent implements OnInit {
 
   async delete(item: BookmarkNode) {
     await chrome.bookmarks.remove(item.id);
-    location.reload(); // Refresh the popup
+    location.reload();
+  }
+
+  async update(item: BookmarkNode) {
+    await chrome.bookmarks.update(item.id, {
+      title: this.editedItemTitle,
+    });
+    location.reload();
+  }
+
+  toggleEditor(item: BookmarkNode) {
+    this.bookmarks = this.bookmarks.map((bookmark) => {
+      if (item.id === bookmark.id) {
+        this.editedItemTitle = item.title;
+        return { ...bookmark, isEditable: !item.isEditable };
+      }
+
+      return bookmark;
+    });
   }
 }
 
@@ -133,6 +197,7 @@ export interface BookmarkNode {
   index: number;
   parentId: string;
   title: string;
+  isEditable: boolean;
   url?: string | null;
   dateLastUsed?: number | null;
 }
